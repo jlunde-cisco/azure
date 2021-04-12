4624f234e41c4df96d46e530febcad6e88799c77
jlunde-cisco <75453543+jlunde-cisco@users.noreply.github.com>
2021-04-12 10:53:21 -0500

    Add files via upload

---
+++ cs.ps1
@@ -0,0 +1,5 @@
+﻿mkdir C:\CustomExtensions
+Invoke-WebRequest -Uri "https://github.com/jlunde-cisco/azure/blob/main/greenshot.exe" -OutFile "C:\CustomExtensions\greenshot.exe"
+& C:\CustomExtensions\greenshot.exe /lang=English /verysilent
+Invoke-WebRequest -Uri "https://github.com/jlunde-cisco/azure/blob/main/tet.ps1" -OutFile "C:\CustomExtensions\tet.ps1"
+& C:\CustomExtensions\tet.ps1
