9978eb7ab76b730faaa170175cd12383c8b7cafe
jlunde-cisco <75453543+jlunde-cisco@users.noreply.github.com>
2021-04-12 14:41:07 -0500

    Update cs.ps1

--- cs.ps1
+++ cs.ps1
@@ -1,5 +1,6 @@
-﻿mkdir C:\CustomExtensions
-Invoke-WebRequest -Uri "https://github.com/jlunde-cisco/azure/blob/main/greenshot.exe" -OutFile "C:\CustomExtensions\greenshot.exe"
+mkdir C:\CustomExtensions
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri "https://github.com/jlunde-cisco/azure/raw/main/greenshot.exe" -OutFile "C:\CustomExtensions\greenshot.exe"
 & C:\CustomExtensions\greenshot.exe /lang=English /verysilent
-Invoke-WebRequest -Uri "https://github.com/jlunde-cisco/azure/blob/main/tet.ps1" -OutFile "C:\CustomExtensions\tet.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/jlunde-cisco/azure/main/tet.ps1" -OutFile "C:\CustomExtensions\tet.ps1"
 & C:\CustomExtensions\tet.ps1
